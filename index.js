c90f1deb0ccb89977138bc05d01e3eeb7138a3d2
Finn <derfinn@gmail.com>
2014-08-27 13:35:58 +0200

    even easier..

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 var request = require('request')
 var unzip = require('unzip')
-var csv = require('csv-parser')
-var ldj = require('ldjson-stream')
 
 var source = 'https://archive.ics.uci.edu/ml/machine-learning-databases/00275/Bike-Sharing-Dataset.zip'
 var datasetname = 'day.csv' // alternatively hour.csv
@@ -9,11 +7,8 @@ var datasetname = 'day.csv' // alternatively hour.csv
 request(source)
   .pipe(unzip.Parse())
   .on('entry', function (entry) {
-    if(entry.path === datasetname) {
-      entry
-        .pipe(csv())
-        .pipe(ldj.serialize())
-        .pipe(process.stdout)
-    }
-    else entry.autodrain()
+    if(entry.path === datasetname) 
+      entry.pipe(process.stdout)
+    else
+      entry.autodrain()
   })
--- readme.md
+++ readme.md
@@ -3,8 +3,15 @@
 Import the static UCI bike sharing dataset into dat with
 
 ```
-node index.js | dat import --json
+node index.js | dat import --csv
 ```
+## Alternative way with temporary file
+
+```
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/00275/Bike-Sharing-Dataset.zip
+unzip -p "Bike-Sharing-Dataset.zip" "day.csv" | dat import --csv
+```
+
 
 ## Source
 https://archive.ics.uci.edu/ml/datasets/Bike+Sharing+Dataset
